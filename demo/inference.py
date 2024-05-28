d67f4110703abf1c3b62b50935847be8f55aa819
tu7 <tu7pham7@gmail.com>
2024-05-28 21:52:20 +0700

    modified:   demo/inference.py

--- demo/inference.py
+++ demo/inference.py
@@ -8,10 +8,14 @@ import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 from data_preprocessing.get_tools import clean_text_vanilla
 
-from tensorflow.keras.preprocessing.text import Tokenizer, tokenizer_from_json
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
 from keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
 
+with open('model_checkpoint/tokenizer.json') as f:
+    data = json.load(f)
+    tokenizer = tokenizer_from_json(data)
+    
 def query(comments, model, tokenizer):
     comments = [clean_text_vanilla(comment) for comment in comments]
     sequences = tokenizer.texts_to_sequences(comments)
@@ -20,9 +24,6 @@ def query(comments, model, tokenizer):
 
 if __name__ == "__main__":
     model = load_model('model_checkpoint/glove_lstm.keras')
-    with open('model_checkpoint/tokenizer.json') as f:
-        data = json.load(f)
-        tokenizer = tokenizer_from_json(data)
     comments = input().split('\n')
     print(comments)
     print(query(comments, model, tokenizer))
