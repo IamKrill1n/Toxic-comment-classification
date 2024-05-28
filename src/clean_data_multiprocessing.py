3488e3b32bb54bfc3d086c4ebc5b9e16f9f73087
tu7 <tu7pham7@gmail.com>
2024-05-28 12:47:56 +0700

    refactor

--- src/clean_data_multiprocessing.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing as mp
-import pandas as pd
-
-from src.get_tools import clean_text_light
-
-def clean_data_multiprocessing(df: 'pd.DataFrame') -> None:
-    with mp.Pool(mp.cpu_count()) as pool:
-        df['cleaned_data'] = pool.map(clean_text_light, df['comment_text'])
