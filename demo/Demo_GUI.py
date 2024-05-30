3c731614b63244b90be7c904177cf8ed048cc1c7
your_username <117010350+lht11062004@users.noreply.github.com>
2024-05-31 02:36:32 +0700

    Update Demo_GUI.py

--- demo/Demo_GUI.py
+++ demo/Demo_GUI.py
@@ -1,4 +1,6 @@
 import gradio as gr
+import pandas as pd 
+import plotly.express as px 
 from HybridRNN import HybridRNN
 from LogisticRegression import LogisticRegression
 from NaiveBayes import NaiveBayes
@@ -19,22 +21,49 @@ rnn = RNN()
 svm = SVM()
 print('Models loaded.')
 
+def create_dataframe(classes, probabilities):
+    dataframe =  pd.DataFrame(
+        {
+            "Classes": classes,
+            "Toxicity probabilities": probabilities
+            
+        }
+    )
+    
+    dataframe['Color'] = ['red' if proba >= 50 else 'blue' for proba in probabilities]
+    return dataframe 
+    
+classes = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 # Define the predict function
 def toxicity_proba(comment, model):
     if model == "Hybrid RNN":
-        return hrnn.predict(comment)
+        dataframe = create_dataframe(classes, [float("{:.4f}".format(proba))*100 for proba in hrnn.predict(comment).tolist()])
+        return px.bar(dataframe, x='Classes', y='Toxicity probabilities', color='Color', color_discrete_map={'red': 'red', 'blue': 'blue'})
+    
     elif model == "Logistic Regression":
-        return lore.predict(comment)
+        dataframe = create_dataframe(classes, [float("{:.4f}".format(proba))*100 for proba in lore.predict(comment).tolist()])
+        return px.bar(dataframe, x='Classes', y='Toxicity probabilities', color='Color', color_discrete_map={'red': 'red', 'blue': 'blue'})
+    
     elif model == "Naive Bayes":
-        return nb.predict(comment)
+        dataframe = create_dataframe(classes, [float("{:.4f}".format(proba))*100 for proba in nb.predict(comment).tolist()])
+        return px.bar(dataframe, x='Classes', y='Toxicity probabilities', color='Color', color_discrete_map={'red': 'red', 'blue': 'blue'})
+    
     elif model == "NBSVM":
-        return nbsvm.predict(comment)
+        dataframe = create_dataframe(classes, [float("{:.4f}".format(proba))*100 for proba in nbsvm.predict(comment).tolist()])
+        return px.bar(dataframe, x='Classes', y='Toxicity probabilities', color='Color', color_discrete_map={'red': 'red', 'blue': 'blue'})
+    
     elif model == 'NBLR':
-        return nblr.predict(comment)
+        dataframe = create_dataframe(classes, [float("{:.4f}".format(proba))*100 for proba in nblr.predict(comment).tolist()])
+        return px.bar(dataframe, x='Classes', y='Toxicity probabilities', color='Color', color_discrete_map={'red': 'red', 'blue': 'blue'})
+    
     elif model == 'RNN':
-        return rnn.predict(comment)
+        dataframe = create_dataframe(classes, [float("{:.4f}".format(proba))*100 for proba in rnn.predict(comment).tolist()])
+        return px.bar(dataframe, x='Classes', y='Toxicity probabilities', color='Color', color_discrete_map={'red': 'red', 'blue': 'blue'})
+    
     elif model == "SVM":
-        return svm.predict(comment)
+        dataframe = create_dataframe(classes, [float("{:.4f}".format(proba))*100 for proba in svm.predict(comment).tolist()])
+        return px.bar(dataframe, x='Classes', y='Toxicity probabilities', color='Color', color_discrete_map={'red': 'red', 'blue': 'blue'})
+
 
 # Define the Gradio interface
 demo = gr.Interface(
@@ -43,7 +72,7 @@ demo = gr.Interface(
         gr.Textbox(label='Comment', placeholder="Enter comment here"),
         gr.Radio(["Hybrid RNN", "Logistic Regression", "Naive Bayes", "NBSVM", "NBLR", "RNN", "SVM"], label="Model")
     ],
-    outputs=gr.Textbox(label="Result")
+    outputs=gr.Plot()
 )
 
 # Launch the interface
