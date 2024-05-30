88cd35c12f9b8658b2e3d5235b649b5e3455630a
thanh309 <>
2024-05-30 15:33:49 +0700

    changes

--- src/blender/HillclimbBlender.py
+++ src/blender/HillclimbBlender.py
@@ -163,8 +163,8 @@ class HillclimbBlender:
 
 def main() -> None:
 
-    blender = HillclimbBlender(model_num=[1, 2, 3, 4, 5, 9])
-    blender.run(iter=100)
+    blender = HillclimbBlender(model_num=[1, 2, 3, 4, 5, 6, 7, 8, 9])
+    blender.run(iter=150)
     print()
 
 if __name__ == '__main__':
