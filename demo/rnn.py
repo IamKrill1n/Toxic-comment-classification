265637ec22e6403a3e05e952c72c5f59f0eb0ff0
tu7 <tu7pham7@gmail.com>
2024-05-30 13:54:38 +0700

    add inference to class

--- demo/rnn.py
+++ demo/rnn.py
@@ -12,18 +12,19 @@ from tensorflow.keras.preprocessing.text import tokenizer_from_json
 from keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
 
-with open('model_checkpoint/tokenizer.json') as f:
-    data = json.load(f)
-    tokenizer = tokenizer_from_json(data)
+class RNN:
+    def __init__(self, model_path, tokenizer_path = 'model_checkpoint/tokenizer.json'):
+        self.model = load_model(model_path)
+        with open(tokenizer_path) as f:
+            data = json.load(f)
+            self.tokenizer = tokenizer_from_json(data)
     
-def query(comments, model, tokenizer):
-    comments = [clean_text_vanilla(comment) for comment in comments]
-    sequences = tokenizer.texts_to_sequences(comments)
-    X = pad_sequences(sequences, maxlen=100)
-    return model.predict([X])
+    def predict(self, comments):
+        comments = [clean_text_vanilla(comment) for comment in comments]
+        sequences = self.tokenizer.texts_to_sequences(comments)
+        X = pad_sequences(sequences, maxlen=100)
+        return self.model.predict([X])
 
 if __name__ == "__main__":
-    model = load_model('model_checkpoint/glove_lstm.keras')
-    comments = input().split('\n')
-    print(comments)
-    print(query(comments, model, tokenizer))
+    model = RNN('model_checkpoint/rnn.keras')
+    print(model.predict(['I fucking love this movie']))
